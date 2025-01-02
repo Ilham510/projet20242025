86f88b20c549af94b7c10acfc58c31425eeb3737
ilham510 <ilham.chikh@univ-rouen.fr>
2025-01-02 20:46:28 +0100

    lecture des fichiers

--- projet.py
+++ projet.py
@@ -1,3 +1,12 @@
 import re
 import matplotlib.pyplot as plt
 
+def lire_fichier_species(filepath):
+    """Lit les noms d'espèces à partir d'un fichier."""
+    with open(filepath, 'r') as file:
+        return [line.strip() for line in file.readlines()]
+
+def lire_fichier_mirna(filepath):
+    """Lit et retourne les lignes du fichier mirna."""
+    with open(filepath, 'r') as file:
+        return file.readlines()
