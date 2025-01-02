0b43b95b046c0a59a63696ff2ceba4ca77f67b15
ilham510 <ilham.chikh@univ-rouen.fr>
2025-01-02 20:42:48 +0100

    script test unitaire v0

---
+++ testunit.py
@@ -0,0 +1,10 @@
+import unittest
+
+class TestProjet(unittest.TestCase):
+    def test_calculer_pourcentages(self):
+        sequence = "AAACCCGGGUU"
+        expected = {'A': 30.0, 'C': 30.0, 'G': 30.0, 'U': 10.0}
+        self.assertEqual(calculer_pourcentages(sequence), expected)
+
+if __name__ == '__main__':
+    unittest.main()
